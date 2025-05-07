cdf3e0d9623a390471ff2ab238fa85eb932000c8
rmfranken <robin.franken@epfl.ch>
2025-05-07 16:51:18 +0200

    fix:spellingmistake

--- src/strings2things/fuzon.py
+++ src/strings2things/fuzon.py
@@ -4,7 +4,7 @@ def extract_relevant_terms(ontology, query, n=5):
     """
     Extract relevant terms from a list of terms based on a query string.
     
-    :param terms: List of terms to search through.
+    :param ontology: pointer to the location of the ontology (can be a path or url).
     :param query: Query string to match against the terms.
     :param n: Number of top matches to return.
     :return: List of tuples containing the matched term and its score.
